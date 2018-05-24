9ebe80905aecd8400c84a9fd1369ab28c636eb1e
ArleyQuin <boomm@bk.ru>
2018-05-24 14:22:02 +0300

    Проверка logget

--- addressbook_web_test/addressbook_web_test/AppManager/HelperLogin.cs
+++ addressbook_web_test/addressbook_web_test/AppManager/HelperLogin.cs
@@ -48,9 +48,14 @@ namespace AddressbookWebTest
         public bool IsLoggedIn(DataAccount account)
         {
             return IsLoggedIn()
-            && driver.FindElement(By.Name("logout")).FindElement(By.TagName("b")).Text == "(" + account.Login + ")";
+                && GetLoggetUserName() == account.Login;
         }
 
-
+        private string GetLoggetUserName()
+        {
+            string text = driver.FindElement(By.Name("logout")).FindElement(By.TagName("b")).Text;
+            return text.Substring(1, text.Length - 2);
+                
+        }
     }
 }
